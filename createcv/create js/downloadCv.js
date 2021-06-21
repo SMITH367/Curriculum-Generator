244beeee45ab3b3c602d72814042f52876fae9ed
BrayanP <brayansmithpuente@gmail.com>
2021-06-20 23:18:36 -0500

    added preview resposnive complete

--- createcv/create js/downloadCv.js
+++ createcv/create js/downloadCv.js
@@ -1,13 +1,20 @@
 const downloadCv = document.querySelector(".downloadCv");
 
+
+
 downloadCv.addEventListener("click",(e)=>{
     e.preventDefault()
     const conf = confirm("¿Desea guardar el curriculum?")
 
     if(conf === true){
-      const finalcv = document.getElementById("final-cv");
+      const finalcv = document.getElementById("final-cv")
+      finalcv.classList.replace("final-cv","font-normal")
       html2pdf()
      .from(finalcv)
      .save()
+     setTimeout(function(){ finalcv.classList.replace("font-normal","final-cv") }, 100);
+
     }
+
+    
 })
