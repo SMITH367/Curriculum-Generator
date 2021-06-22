4358247776018720a3226c258984df2f56bd9465
BrayanP <brayansmithpuente@gmail.com>
2021-06-22 00:10:27 -0500

    spaces disegn for templates

--- createcv/create js/drawProffesional.js
+++ createcv/create js/drawProffesional.js
@@ -7,7 +7,7 @@ profile.addEventListener("input",()=>{
     let contProfile = document.querySelector(".profile")
     let value = profile.value
     contProfile.innerHTML = ` <h3> 
-    <i class="fa fa-user-plus" aria-hidden="true"></i> Perfil </h3> <p> ${value} <p> `
+    <i class="fa fa-user-plus" aria-hidden="true"></i> Perfil </h3> <br> <p> ${value} <p> `
 
 })
 
@@ -16,11 +16,11 @@ experience.addEventListener("input",()=>{
     let contExperience = document.querySelector(".experience")
     let value = experience.value
     contExperience.innerHTML = ` <h3> 
-    <i class="fa fa-address-card" aria-hidden="true"></i> Experiencia</h3> <p> ${value} <p> `
+    <i class="fa fa-address-card" aria-hidden="true"></i> Experiencia</h3> <br> <p> ${value} <p> `
 
 })
 education.addEventListener("input",()=>{
     let contEducation = document.querySelector(".education")
     let value = education.value
-    contEducation.innerHTML = ` <h3>  <i class="fa fa-university" aria-hidden="true"></i> Educacion </h3> <p> ${value} <p>`
+    contEducation.innerHTML = ` <h3>  <i class="fa fa-university" aria-hidden="true"></i> Educacion </h3> <br> <p> ${value} <p>`
 })
