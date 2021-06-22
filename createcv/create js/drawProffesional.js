bb8375a0702bc24efe7f60668219302ce2543af5
BrayanP <brayansmithpuente@gmail.com>
2021-06-21 23:51:15 -0500

    adjust template3

--- createcv/create js/drawProffesional.js
+++ createcv/create js/drawProffesional.js
@@ -6,7 +6,7 @@ const education = document.getElementById("education")
 profile.addEventListener("input",()=>{
     let contProfile = document.querySelector(".profile")
     let value = profile.value
-    contProfile.innerHTML = ` <h3 class="center"> 
+    contProfile.innerHTML = ` <h3> 
     <i class="fa fa-user-plus" aria-hidden="true"></i> Perfil </h3> <p> ${value} <p> `
 
 })
@@ -15,12 +15,12 @@ experience.addEventListener("input",()=>{
   
     let contExperience = document.querySelector(".experience")
     let value = experience.value
-    contExperience.innerHTML = ` <h3 class="center"> 
+    contExperience.innerHTML = ` <h3> 
     <i class="fa fa-address-card" aria-hidden="true"></i> Experiencia</h3> <p> ${value} <p> `
 
 })
 education.addEventListener("input",()=>{
     let contEducation = document.querySelector(".education")
     let value = education.value
-    contEducation.innerHTML = ` <h3 class="center">  <i class="fa fa-university" aria-hidden="true"></i> Educacion </h3> <p> ${value} <p>`
+    contEducation.innerHTML = ` <h3>  <i class="fa fa-university" aria-hidden="true"></i> Educacion </h3> <p> ${value} <p>`
 })
