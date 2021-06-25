408e4ed3a6d077a83e9326a32a210712984998ae
BrayanP <brayansmithpuente@gmail.com>
2021-06-24 23:58:02 -0500

    added slider, buton efect, footer etc

--- Js/main.js
+++ Js/main.js
@@ -1,26 +1,22 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//Nav mobile 
 const navToogle = document.querySelector(".toogle-init");
 const navMenu = document.querySelector(".nav-menu");
 const sidebar = document.querySelector(".toogle");
 
-navToogle.addEventListener("click",function() {
+navToogle.addEventListener("click", function () {
     navMenu.classList.toggle("nav-phone-visible");
     sidebar.classList.toggle("hidden");
-  
-   
+
+
 })
+
+//Slider home
+
+$(function () {
+    $('#slider div:gt(0)').hide();
+    setInterval(function () {
+        $('#slider div:first-child').fadeOut(0)
+            .next('div').fadeIn(1000)
+            .end().appendTo('#slider');
+    }, 3000);
+});
