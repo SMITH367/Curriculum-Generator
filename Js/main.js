ffe2e2874b0f4fec68861e6cc3b5b889098a8053
BrayanP <brayansmithpuente@gmail.com>
2021-06-26 00:10:53 -0500

    fixed bug  toogle button in -mainpage

--- Js/main.js
+++ Js/main.js
@@ -16,7 +16,7 @@ $(function () {
     $('#slider div:gt(0)').hide();
     setInterval(function () {
         $('#slider div:first-child').fadeOut(0)
-            .next('div').fadeIn(900)
+            .next('div').fadeIn(1000)
             .end().appendTo('#slider');
     }, 3000);
 });
