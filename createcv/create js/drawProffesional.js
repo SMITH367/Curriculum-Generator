ff858c9cd624dc4fd12b57687b08bdb816bca3a0
BrayanP <brayansmithpuente@gmail.com>
2021-06-26 00:58:58 -0500

    refactoring of drawProffesional

--- createcv/create js/drawProffesional.js
+++ createcv/create js/drawProffesional.js
@@ -2,25 +2,33 @@ const profile = document.getElementById("profile")
 const experience = document.getElementById("experience")
 const education = document.getElementById("education")
 
+class drawAbout {
+    constructor() {}
 
-profile.addEventListener("input",()=>{
-    let contProfile = document.querySelector(".profile")
-    let value = profile.value
-    contProfile.innerHTML = ` <h3> 
-    <i class="fa fa-address-card" aria-hidden="true"></i> Perfil </h3> <br> <p> ${value} <p> `
-
-})
-
-experience.addEventListener("input",()=>{
-  
-    let contExperience = document.querySelector(".experience")
-    let value = experience.value
-    contExperience.innerHTML = ` <h3> 
-    <i class="fa fa-address-card" aria-hidden="true"></i> Experiencia</h3> <br> <p> ${value} <p> `
-
-})
-education.addEventListener("input",()=>{
-    let contEducation = document.querySelector(".education")
+    drawProfile() {
+        let contProfile = document.querySelector(".profile")
+        let value = profile.value
+        contProfile.innerHTML = ` <h3> 
+        <i class="fa fa-address-card" aria-hidden="true"></i> Perfil </h3> <br> <p> ${value} <p> `
+    }
+    drawExperience() {
+        let contExperience = document.querySelector(".experience")
+        let value = experience.value
+        contExperience.innerHTML = ` <h3> 
+        <i class="fa fa-address-card" aria-hidden="true"></i> Experiencia</h3> <br> <p> ${value} <p> `
+    }
+    drawEducation(){
+        let contEducation = document.querySelector(".education")
     let value = education.value
     contEducation.innerHTML = ` <h3>  <i class="fa fa-university" aria-hidden="true"></i> Educacion </h3> <br> <p> ${value} <p>`
-})
+    }
+}
+
+const drawProffesional = new drawAbout()
+
+profile.addEventListener("input", () => drawProffesional.drawProfile())
+
+experience.addEventListener("input", () => drawProffesional.drawExperience())
+
+education.addEventListener("input", () => drawProffesional.drawEducation())
+
